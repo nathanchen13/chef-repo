743fcba276ffb687fb64258dee660b23fd941073
Coveros-NChen <Coveros-NChen>
2016-06-28 09:53:39 -0400

    reconfig the ip in knife

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,5 +5,5 @@ log_level                :info
 log_location             STDOUT
 node_name                "nchen"
 client_key               "#{current_dir}/nchen.pem"
-chef_server_url          "https://ec2-52-91-120-142.compute-1.amazonaws.com/organizations/coveros"
+chef_server_url          "https://172.31.2.19/organizations/coveros"
 cookbook_path            ["#{current_dir}/../cookbooks"]
