d68c21bc3647076907343a397772b378b677b02d
Coveros-NChen <Coveros-NChen>
2016-06-29 11:08:55 -0400

    update

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,5 +6,5 @@ log_location             STDOUT
 node_name                "nchen"
 client_key               "#{current_dir}/nchen.pem"
 chef_server_url          "https://172.31.2.19/organizations/coveros"
-cookbook_path            ["#{current_dir}/../cookbooks"]
-
+#cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_path [ '.', '..', './cookbooks', '~/git/chef' ]
